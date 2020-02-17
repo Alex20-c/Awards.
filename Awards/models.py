1faa6b947e7ee9e23d43663e9ed75c2c15d00d55
Alex Kiprop <kipropalex59@gmail.com>
2020-02-17 08:02:11 +0300

    create a profile class that returns its details

--- Awards/models.py
+++ Awards/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=100)
@@ -44,3 +43,23 @@ class Project(models.Model):
 
     class Meta:
         ordering = ['-id']
+class Profile(models.Model):
+    profile_picture = models.ImageField(upload_to='prof_pics/',blank=True)
+    prof_user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    bio = models.TextField()
+    contact_info = models.CharField(max_length=200,blank=True)
+    profile_Id = models.IntegerField(default=0)
+    all_projects = models.ForeignKey('Project',on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_bio(self,bio):
+        self.bio = bio
+        self.save()
